084ecc804f9dfae582cbbad7ce9639353816c9b0
kamel <kamel.soubra@gmail.com>
2025-09-30 17:29:04 +0300

    initialized database

---
+++ database.py
@@ -0,0 +1,141 @@
+import sqlite3
+
+
+def connect_to_db():
+    conn = sqlite3.connect('database.db')
+    return conn
+
+
+def create_db_table():
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                address TEXT NOT NULL,
+                country TEXT NOT NULL
+            );
+        ''')
+        conn.commit()
+        print("User table created successfully")
+    except Exception:
+        print("User table creation failed - Maybe table exists")
+    finally:
+        conn.close()
+
+
+def insert_user(user):
+    inserted_user = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO users (name, email, phone, address, country) VALUES (?, ?, ?, ?, ?)",
+            (user['name'], user['email'], user['phone'], user['address'], user['country'])
+        )
+        conn.commit()
+        inserted_user = get_user_by_id(cur.lastrowid)
+    except Exception:
+        conn.rollback()
+    finally:
+        conn.close()
+    return inserted_user
+
+
+def get_users():
+    users = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users")
+        rows = cur.fetchall()
+        # convert row objects to dictionaries
+        for i in rows:
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
+    except Exception:
+        users = []
+    finally:
+        try:
+            conn.close()
+        except Exception:
+            pass
+    return users
+
+
+def get_user_by_id(user_id):
+    user = {}
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+        row = cur.fetchone()
+        if row:
+            # convert row object to dictionary
+            user = {
+                "user_id": row["user_id"],
+                "name": row["name"],
+                "email": row["email"],
+                "phone": row["phone"],
+                "address": row["address"],
+                "country": row["country"],
+            }
+    except Exception:
+        user = {}
+    finally:
+        try:
+            conn.close()
+        except Exception:
+            pass
+    return user
+
+
+def update_user(user):
+    updated_user = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE users SET name = ?, email = ?, phone = ?, address = ?, country = ? WHERE user_id = ?",
+            (user["name"], user["email"], user["phone"], user["address"], user["country"], user["user_id"])
+        )
+        conn.commit()
+        # return the user
+        updated_user = get_user_by_id(user["user_id"])
+    except Exception:
+        conn.rollback()
+        updated_user = {}
+    finally:
+        conn.close()
+    return updated_user
+
+
+def delete_user(user_id):
+    message = {}
+    try:
+        conn = connect_to_db()
+        conn.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        conn.commit()
+        message["status"] = "User deleted successfully"
+    except Exception:
+        conn.rollback()
+        message["status"] = "Cannot delete user"
+    finally:
+        conn.close()
+    return message
+
+
+if __name__ == "__main__":
+    create_db_table()
+    print("Database initialized.")
